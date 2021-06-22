6f88effc3b1a966c69b1420da67602491f925f47
audreysanchez <audrey9608@gmail.com>
2021-06-22 01:17:23 -0500

    Add testCNN.py

---
+++ testCNN.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile as wav
+from scipy.fftpack import fft, fftfreq
+from scipy.misc import electrocardiogram
+from scipy.io.wavfile import write
+from scipy.signal import find_peaks
+from time import sleep
+from keras import preprocessing
+from keras.models import load_model
+from glob import glob
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+tf.get_logger().warning('test')
+# WARNING:tensorflow:test
+tf.get_logger().setLevel('ERROR')
+tf.get_logger().warning('test')
+# (silence)
+
+
+#CNN Model will determine if picture is of a drone
+
+def cnn_dronePred(img_test):
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    class_names = ['DRONE', 'NO_DRONE']
+    width = 96
+    height = 96
+
+    model = load_model('drone_detector.h5')
+
+    base_path_img = "static/images"
+    images = []
+    path = os.path.join(base_path_img, img_test)
+
+    for image_path in glob(path):
+        img = preprocessing.image.load_img(image_path, target_size=(width, height))
+
+    img_X = np.expand_dims(img, axis=0)
+
+    predictions = model.predict(img_X)
+    result = class_names[np.argmax(predictions)]
+
+    print('The type predicted is: {}'.format(result))
+
+    return result
