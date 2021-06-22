05f1f5011e0411c6aa2d0e705ddf8498499dd696
audreysanchez <audrey9608@gmail.com>
2021-06-22 01:11:17 -0500

    Add app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from testCNN import cnn_dronePred
+from flask import Flask, render_template, request
+import os, random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    img_dir = os.listdir("static/images")
+    randDrone = random.choice(img_dir)
+    print(randDrone)
+
+    result = cnn_dronePred(randDrone)
+    
+    if(result == "DRONE"):
+        print("Drone present on property")
+
+    else:
+        print("No Drone on property")
+
+
+    return render_template("index.html", d=randDrone, r=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
