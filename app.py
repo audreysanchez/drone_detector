f7b729748b1b30858faa7e6f7ecdc7d622c7aa82
Audrey Sanchez <4109641+audreysanchez@users.noreply.github.com>
2021-06-22 23:05:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def main():
     return render_template("index.html", d=randDrone, r=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
