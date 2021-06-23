db48c3cd399f600dcedb4aa6ed0b89d37a092381
Audrey Sanchez <4109641+audreysanchez@users.noreply.github.com>
2021-06-22 22:29:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request
 import os, random
 
 app = Flask(__name__)
-
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route("/")
 def main():
